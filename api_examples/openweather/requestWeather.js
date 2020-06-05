1562f64f212df66f99663708e2633cab7d67b03b
Patrick Tran <patrick.tran06@gmail.com>
2020-06-04 21:54:39 -0700

    refactor; saveWeather now has a default

--- api_examples/openweather/requestWeather.js
+++ api_examples/openweather/requestWeather.js
@@ -11,9 +11,6 @@ async function getWeather(city) {
   }
 }
 
-async function checkDB(city) {
-}
-
 async function requestWeather(city) {
   try {
     const API_KEY = "9e3f9ebc8da776c7cc6f8f06b80a9203";
@@ -43,14 +40,14 @@ const pool = new Pool({
   port: 5432,
 })
 
-async function saveWeather(request_payload, response_payload) {
+async function saveWeather(request_payload, response_payload, name="openweather") {
   const queryText = `
     INSERT INTO api_responses 
       (name, request_payload, response_payload) 
     VALUES($1, $2, $3) 
     RETURNING *`;
   const queryValues = [
-    'openweather', 
+    name, 
     JSON.stringify(request_payload), 
     JSON.stringify(response_payload)
   ];
@@ -64,26 +61,10 @@ async function saveWeather(request_payload, response_payload) {
 }
 
 async function testSaveWeather(city, temperature, humidity) {
-  try {
-    var city = city || "San Francisco"
-    var temperature = temperature || Math.floor(Math.random() * 100)
-    var humidity = humidity || Math.floor(Math.random() * 100)
-    const queryText = `
-      INSERT INTO api_responses 
-        (name, request_payload, response_payload) 
-      VALUES($1, $2, $3) 
-      RETURNING *`
-    const queryValues = [
-      'testing_openweather', 
-      `{"city": "${city}"}`, 
-      `{"temperature": "${temperature}", "humidity": ${humidity}}`
-    ]
-    let results = await pool.query(queryText, queryValues);
-    let rows = results.rows
-    console.log(rows)
-  } catch (err) {
-    console.log(err)
-  }
+  var city = city || "San Francisco"
+  var temperature = temperature || Math.floor(Math.random() * 100)
+  var humidity = humidity || Math.floor(Math.random() * 100)
+  return await saveWeather({ city }, { temperature, humidity }, 'testing_saving')
 }
 
 async function getSavedWeather(city, recent=false) {
@@ -104,7 +85,6 @@ async function getSavedWeather(city, recent=false) {
 async function print(promise) {
   promise.then(x => console.log(x))
 }
-
 
 module.exports = { 
   requestWeather, 
